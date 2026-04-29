21e1279ea851ba6cc0d60c1a8b4587b1e763f2e6
Kwaye Kant <gabrielkwaye@gmail.com>
2026-04-29 20:10:37 +0100

    Hide the 'problematique' content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
       <Hero />
       <ValueGrid />
       <StrategicPillars />
-      <ProblemClustersPreview />
+      {/* <ProblemClustersPreview /> */}
       <RseFramework />
       <ActorsEcosystem />
       <MethodologyMap />
