5802b7097cfbd7bc9578375da933df03a06054a8
Kwaye Kant <gabrielkwaye@gmail.com>
2026-08-03 17:01:24 +0100

    Set Cookies to proper name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -57,8 +57,9 @@ export async function getAuthUserFromRequest(req: NextRequest): Promise<AuthUser
 
 export async function getCurrentUser(): Promise<AuthUser | null> {
   const cookieStore = await cookies()
-  const token = cookieStore.get(AUTH_COOKIE_NAME)?.value
-  console.log('Current user token:', "cookieStore.get(AUTH_COOKIE_NAME)") // Debugging line
+  // const token = cookieStore.get(AUTH_COOKIE_NAME)?.value
+  const token = cookieStore.get("pak_token")?.value
+  console.log('Current user token:', token) // Debugging line
   if (!token) return null
   console.log('Token found:', token) // Debugging line
 
